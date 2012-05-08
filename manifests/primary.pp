3d127dabe2b292c7022668f693f1bdbb6a7319d7
Cody Herriges <c.a.herriges@gmail.com>
2012-05-08 15:28:45 -0700

    Forgot to remove pe from dependencies

--- manifests/primary.pp
+++ manifests/primary.pp
@@ -99,8 +99,8 @@ class haca::primary {
   }
 
   cs_colocation { 'ca_vip_ca_service':
-    primitives => [ 'ca_vip', 'pe_ca_service' ],
-    require    => Cs_primitive[[ 'pe_ca_service', 'ca_vip' ]],
+    primitives => [ 'ca_vip', 'ca_service' ],
+    require    => Cs_primitive[[ 'ca_service', 'ca_vip' ]],
   }
   cs_order { 'ca_vip_ca_service':
     first   => 'ca_vip',
@@ -108,8 +108,8 @@ class haca::primary {
     require => Cs_colocation['ca_vip_ca_service'],
   }
   cs_colocation { 'ms_kicker_ca_service':
-    primitives => [ 'ms_kicker', 'pe_ca_service' ],
-    require     => Cs_primitive[[ 'pe_ca_service', 'ms_kicker' ]],
+    primitives => [ 'ms_kicker', 'ca_service' ],
+    require     => Cs_primitive[[ 'ca_service', 'ms_kicker' ]],
   }
   cs_order { 'ms_kicker_ca_service':
     first   => 'ca_service',
