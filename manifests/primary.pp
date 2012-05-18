3d45c6e38c7d2afd23d839b30b99b2c0ecc27956
Cody Herriges <c.a.herriges@gmail.com>
2012-05-17 20:35:27 -0700

    try the reverse

--- manifests/primary.pp
+++ manifests/primary.pp
@@ -55,7 +55,6 @@ class haca::primary {
     client      => false,
     accept      => '1873',
     connect     => '873',
-    subscribe   => Rsync::Server::Module['ca']
   }
 
   cs_property { 'stonith-enabled':
@@ -139,7 +138,7 @@ class haca::primary {
     incoming_chmod => false,
     outgoing_chmod => false,
     lock_file      => '/var/run/rsyncd.lock',
-
+    subscribe      => Stunnel::Tun['rsyncd']
   }
 
   cron { 'pull_ca':
