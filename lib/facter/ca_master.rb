71a9d31accf92fa2e29c0a373fde99e74f0b6e01
Cody Herriges <c.a.herriges@gmail.com>
2012-05-17 23:03:23 -0700

    picking up some extra whitespace

--- lib/facter/ca_master.rb
+++ lib/facter/ca_master.rb
@@ -4,7 +4,7 @@ Facter.add(:ca_master) do
     command = 'crm resource status'
     output = Facter::Util::Resolution.exec(command)
     if output
-      master = output.scan(/\s+Master\/Slave.*\n\W.+Masters:\W\[\W([a-z,A-Z,0-9,\.-].*)\]/).to_s
+      master = output.scan(/\s+Master\/Slave.*\n\W.+Masters:\W\[\W([a-z,A-Z,0-9,\.-].*)\]/).to_s.chop
       master
     end
   end
